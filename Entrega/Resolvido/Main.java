b9ac6cc333bf4d8b285cdcd076a7b7b3e7d4c673
thiagosonego <74835122+thiagosonego@users.noreply.github.com>
2024-12-14 11:51:50 -0300

    Delete Entrega/Resolvido directory

--- Entrega/Resolvido/Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        Sistema sistema = new Sistema();
-        sistema.inicializarSistema();
-        sistema.menuPrincipal();
-    }
-}
